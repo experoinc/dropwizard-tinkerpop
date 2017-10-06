d8935608718eb1dbbe6dfcd50721a0f781489fa7
Chris Pounds <chrislbs@gmail.com>
2017-10-06 17:26:47 -0500

    raging

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -9,7 +9,7 @@ if [ "${TRAVIS_BRANCH}" = 'master' ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ];
     gpg --fast-import codesigning.asc
 
     gpg --version
-    echo ${gPG_PASSPHRASE} | gpg -u ${GPG_KEY_NAME} --output test.out --passphrase-fd 0 --sign test.txt
+    echo ${GPG_PASSPHRASE} | gpg --no-tty -u ${GPG_KEY_NAME} --output test.out --passphrase-fd 0 --sign test.txt
     gpg -u ${GPG_KEY_NAME} --output test.dec --decrypt test.out
     ls -alh
     cat test.dec
