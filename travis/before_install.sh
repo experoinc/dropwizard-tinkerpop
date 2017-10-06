7a01f596079682e87941b994ae60aca5d16b5b56
Chris Pounds <chrislbs@gmail.com>
2017-10-06 17:25:19 -0500

    not fun

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -8,9 +8,8 @@ if [ "${TRAVIS_BRANCH}" = 'master' ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ];
 
     gpg --fast-import codesigning.asc
 
-    cat ${HOME}/.gnupg/gpg.conf
     gpg --version
-    gpg -u ${GPG_KEY_NAME} --output test.out --passphrase ${GPG_PASSPHRASE} --sign test.txt
+    echo ${gPG_PASSPHRASE} | gpg -u ${GPG_KEY_NAME} --output test.out --passphrase-fd 0 --sign test.txt
     gpg -u ${GPG_KEY_NAME} --output test.dec --decrypt test.out
     ls -alh
     cat test.dec
