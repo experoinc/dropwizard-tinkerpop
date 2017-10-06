17a23cac9bc865b35bfa76bf0d47459848acc681
Chris Pounds <chrislbs@gmail.com>
2017-10-06 17:29:24 -0500

    more'

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -e
 if [ "${TRAVIS_BRANCH}" = 'master' ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ]; then
   openssl aes-256-cbc -K ${encrypted_d8b618a76e1d_key} \
     -iv ${encrypted_d8b618a76e1d_iv} \
@@ -8,9 +9,13 @@ if [ "${TRAVIS_BRANCH}" = 'master' ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ];
 
     gpg --fast-import codesigning.asc
 
-    gpg --version
+    echo "attempting signature"
     echo ${GPG_PASSPHRASE} | gpg --no-tty -u ${GPG_KEY_NAME} --output test.out --passphrase-fd 0 --sign test.txt
+    echo "finishing signature"
+
+    echo "attempting decrypt"
     gpg -u ${GPG_KEY_NAME} --output test.dec --decrypt test.out
+    echo "finishing decrypt"
     ls -alh
     cat test.dec
 fi
